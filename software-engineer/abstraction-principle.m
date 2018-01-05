1ba80922550a418f1470519b5f8db6cbaff30ed5
Simon Vocella <voxsim@gmail.com>
2018-01-05 16:10:06 +0100

    Create abstraction-principle.m

---
+++ software-engineer/abstraction-principle.m
@@ -0,0 +1,2 @@
+# Abstraction Principle
+Related to [DRY](https://github.com/voxsim/knowledge/blob/master/software-engineer/dont-repeat-yourserlf.md) is the abstraction principle “Each significant piece of functionality in a program should be implemented in just one place in the source code.”
